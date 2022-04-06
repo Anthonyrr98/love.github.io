92c9d08cb57e661c4bdaffea6136c3217af3bdc2
zhaorongli98 <1061965034@qq.com>
2022-04-06 19:07:22 +0800

    更新了彩蛋

---
+++ js/oneyear/typeWriter.js
@@ -0,0 +1,36 @@
+
+let i = 0;
+let text1 = "你好呀！段雅楠！我的女朋友";
+let text2 = "今天是非常特殊的一天"
+let speed = 100;
+
+function typeWriter(text, para){
+	if(ok == 2){
+		clearInterval(typeInterval);
+	}
+	if(i < text.length){
+		document.getElementById(para).innerHTML += text.charAt(i);
+		i++;
+		speed = Math.random() * 50 + 100;
+	}
+	else{
+		if(ok == 0){
+			i = 0;
+		}
+		ok += 1;
+	}
+}
+
+var typeInterval;
+
+//window.onload = function() {
+//	window.onload = function(){};
+   	typeInterval = setInterval(function(){
+		if(ok == 0){
+			typeWriter(text1, "txt1");
+		}
+		else if(ok == 1){
+			typeWriter(text2, "txt2");
+		}
+	}, 100);
+//};
