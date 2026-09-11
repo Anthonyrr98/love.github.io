12e0955a13bcb4ef69fd7084d5e1945ddf7894c6
Anthonyrr <60782320+Anthonyrr98@users.noreply.github.com>
2026-09-12 00:44:48 +0800

    Improve gallery navigation and metadata

--- js/gallerymain.js
+++ js/gallerymain.js
@@ -6,12 +6,90 @@
 
     // Initialize all functions when the document is ready.
 	$(document).ready(function(){
+		initGalleryPage();
 		initResize();
 		initScroller();
 		initAnimation();
 		initIsotope();
 		initContactAjax();
 	});
+
+/* --------------------------------------------------
+	Gallery page semantics and metadata
+-------------------------------------------------- */
+
+	function initGalleryPage () {
+		var pages = {
+			'kaifeng': ['开封', '记录我们在开封旅行时留下的照片与故事'],
+			'xian': ['西安', '记录我们在西安旅行时留下的照片与故事'],
+			'huashan': ['华山', '记录我们一起登华山时留下的照片与故事'],
+			'yuntaishan': ['云台山', '记录我们在云台山旅行时留下的照片与故事'],
+			'guizhou': ['贵州', '记录我们在贵州旅行时留下的照片与故事'],
+			'nanyang': ['南阳', '记录我们在南阳旅行时留下的照片与故事'],
+			'biyejinian': ['毕业纪念', '属于我们的毕业纪念照片'],
+			'luoyang': ['洛阳', '记录我们在洛阳旅行时留下的照片与故事'],
+			'luohe': ['漯河', '记录我们在漯河旅行时留下的照片与故事'],
+			'qinghai': ['青海', '记录我们在青海旅行时留下的照片与故事'],
+			'gansu': ['甘肃', '记录我们在甘肃旅行时留下的照片与故事'],
+			'chengdu': ['成都', '记录我们在成都旅行时留下的照片与故事'],
+			'beijing': ['北京', '记录我们在北京旅行时留下的照片与故事'],
+			'yinchuan': ['银川', '记录我们在银川旅行时留下的照片与故事'],
+			'xin': ['信', '写给彼此的信与共同回忆'],
+			'menpiao': ['门票', '收藏我们一路上留下的门票与纪念'],
+			'2021meishi': ['美食合集', '记录我们一起品尝过的美食']
+		};
+		var slug = (window.location.pathname.split('/').pop() || 'index.html').replace('.html', '');
+		var isLegacyFoodPage = /^202[2-7]meishi$/.test(slug);
+		var page = pages[isLegacyFoodPage ? '2021meishi' : slug];
+		if (!page) return;
+
+		var canonicalSlug = isLegacyFoodPage ? '2021meishi' : slug;
+		var canonicalUrl = 'https://www.zrldyn.com/gallery/' + canonicalSlug + '.html';
+		document.title = page[0] + '相册｜赵荣力和段雅楠的PLOG';
+		$('meta[name="description"]').attr('content', page[1]);
+		setHeadLink('canonical', canonicalUrl);
+		setHeadMeta('property', 'og:title', document.title);
+		setHeadMeta('property', 'og:description', page[1]);
+		setHeadMeta('property', 'og:type', 'website');
+		setHeadMeta('property', 'og:url', canonicalUrl);
+		var $nav = $('.isotopeFilters .filter').first();
+		if ($nav.length && !$nav.find('a[href$="/gallery/"]').length) {
+			$nav.prepend('<li><a class="btn btn-isoFilter" href="https://www.zrldyn.com/gallery/">相册首页</a></li>');
+		}
+
+		// The yearly food pages contain the same collection. Present one honest entry
+		// while keeping the old URLs available for existing bookmarks.
+		var $foodLinks = $('.isotopeFilters a[href*="meishi.html"]');
+		if ($foodLinks.length) {
+			$foodLinks.first().text('美食合集').attr('href', 'https://www.zrldyn.com/gallery/2021meishi.html');
+			$foodLinks.slice(1).closest('li').remove();
+		}
+
+		var $logo = $('.logo img').first();
+		$logo.removeAttr('loading').attr({'fetchpriority': 'high', 'alt': '我要和你一起看世界'});
+		$('.isotopeSelector img').each(function (index) {
+			var $image = $(this);
+			if (!$image.attr('alt')) $image.attr('alt', page[0] + '照片 ' + (index + 1));
+			if (index < 5) {
+				$image.removeAttr('loading').attr('fetchpriority', index === 0 ? 'high' : 'auto');
+			} else {
+				$image.attr('loading', 'lazy').removeAttr('fetchpriority');
+			}
+		});
+	}
+
+	function setHeadLink (rel, href) {
+		var $link = $('link[rel="' + rel + '"]');
+		if (!$link.length) $link = $('<link>').attr('rel', rel).appendTo('head');
+		$link.attr('href', href);
+	}
+
+	function setHeadMeta (attribute, name, content) {
+		var selector = 'meta[' + attribute + '="' + name + '"]';
+		var $meta = $(selector);
+		if (!$meta.length) $meta = $('<meta>').attr(attribute, name).appendTo('head');
+		$meta.attr('content', content);
+	}
 
 /* --------------------------------------------------
 	Resize
@@ -108,14 +186,14 @@
 			else {
 				$('#isotopeShowMore').hide();
 			}
-			if ($('.isotopeSelector').hasClass('visible-xs')) {
-				$('.isotopeSelector').removeClass('visible-xs');
+			if ($('.isotopeSelector').hasClass('gallery-hidden')) {
+				$('.isotopeSelector').removeClass('gallery-hidden');
 			}
 			var index = 0;
 
 			$(itemElems).each(function () {
 				if (index >= initial_items) {
-					$(this).addClass('visible-xs');
+					$(this).addClass('gallery-hidden');
 				}
 				index++;
 			});
@@ -134,11 +212,11 @@
 		
 		// function load more item
 		function showNextItems(pagination) {
-			var itemsMax = $('.visible-xs').length;
+			var itemsMax = $('.gallery-hidden').length;
 			var itemsCount = 0;
-			$('.visible-xs').each(function () {
+			$('.gallery-hidden').each(function () {
 				if (itemsCount < pagination) {
-					$(this).removeClass('visible-xs');
+					$(this).removeClass('gallery-hidden');
 					itemsCount++;
 				}
 			});
@@ -153,7 +231,7 @@
 			var itemsCount = 0;
 			$('.isotopeSelector').each(function () {
 				if (itemsCount >= pagination) {
-					$(this).addClass('visible-xs');
+					$(this).addClass('gallery-hidden');
 				}
 				itemsCount++;
 			});
